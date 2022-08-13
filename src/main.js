8ce63c78727b61835d43477b406928175664671a
IlyasDiker <ilyasbenhssine@gmail.com>
2022-08-13 17:08:33 +0100

    Install Pinia Persist Plugin

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persist'
 
 const pinia = createPinia()
+pinia.use(piniaPersist)
 
 createApp(App)
 .use(router)

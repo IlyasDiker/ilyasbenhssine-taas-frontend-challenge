cf04686081ff4d8ee512fab5b44904ec64dc25bb
IlyasDiker <ilyasbenhssine@gmail.com>
2022-08-13 17:04:41 +0100

    Install Pinia

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+
+createApp(App)
+.use(router)
+.use(pinia)
+.mount('#app')
